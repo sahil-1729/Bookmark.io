5c42808fd2ef8ba1766808d1993221a90652474e
Sah!l <sahil.maji@gmail.com>
2024-07-13 21:15:33 +0530

    add new dependency url-metadata

--- src/components/card.tsx
+++ src/components/card.tsx
@@ -3,22 +3,62 @@
 import { Button } from "./ui/button"
 import { createClient } from "@/utils/supabase/server"
 import { fetchBookmark } from "@/types"
-
-import getMetaData from 'metadata-scraper'
+import urlMetadata from 'url-metadata'
 import { Trash } from "lucide-react"
 
+const options = {
+    // custom request headers
+    requestHeaders: {
+        'User-Agent': 'url-metadata/3.0 (npm module)',
+        'From': 'example@example.com'
+    },
+
+    // `fetch` API cache setting for request
+    cache: 'no-cache',
+
+    // `fetch` API mode (ex: 'cors', 'no-cors', 'same-origin', etc)
+    mode: 'cors',
+
+    // charset to decode response with (ex: 'auto', 'utf-8', 'EUC-JP')
+    // defaults to auto-detect in `Content-Type` header or meta tag
+    // if none found, default `auto` option falls back to `utf-8`
+    // override by passing in charset here (ex: 'windows-1251'):
+    decode: 'auto',
+
+    // timeout in milliseconds, default is 10 seconds
+    timeout: 10000,
+
+    // number of characters to truncate description to
+    descriptionLength: 750,
+
+    // force image urls in selected tags to use https,
+    // valid for images & favicons with full paths
+    ensureSecureImageRequest: true,
+
+    // return raw response body as string
+    includeResponseBody: false,
+
+    // alternate use-case: pass in `Response` object here to be parsed
+    // see example below
+    parseResponseObject: null,
+};
+
 export async function GetData(link: string) {
-    // const getMetaData = require('metadata-scraper')
 
     // async function run() {
     const url = link
     try {
-        const data = await getMetaData(url)
-        console.log('api ', data.title)
-        return data.title
+        const metadata = await urlMetadata(url);
+        console.log(metadata)
+        // const data = await getMetaData(url)
+        // console.log('api ', data.title)
+        return metadata.title
+        // return ""
     } catch (e) {
         console.log(e)
     }
+    return null
+
     // console.log(data)
     // }
 }
