41fdbc0fe49b275cca29efd91d8e66d23e8e8bc6
Sah!l <sahil.maji@gmail.com>
2025-09-09 04:04:59 +0530

    create UI for sharing bookmark feature

---
+++ src/app/share/[slug]/page.tsx
@@ -0,0 +1,95 @@
+
+import DialogForm from "@/components/dialog-form";
+import BookmarkSidebar from "@/components/sidebar/bookmarkSidebar";
+import Navbar from "@/components/navBar";
+import DialogLogin from "@/components/dialog-login-again";
+import DeleteBookmarkBtn from "@/components/delBookmarkBtn";
+import DialogEditBookmark from "@/components/dialogEditBookmark";
+import ToggleVisit from "@/components/toggleVisit";
+import { Button } from "@/components/ui/button";
+import GetBookmark from "@/server-actions/getBookmark";
+import { fetchBookmark } from "@/types";
+import { Folder } from "lucide-react";
+import { redirect } from "next/navigation";
+import Link from "next/link";
+import VerifyToken from "@/server-actions/verifyToken";
+
+// export async function generateStaticParams() {
+//     return [{ "id": 1 }]
+// }
+
+export default async function Category({
+    params,
+    searchParams,
+
+}: {
+    params: Promise<{ slug: string }>,
+    searchParams: { category: string };
+}) {
+    const { slug } = await params
+    const decodedSlug = decodeURIComponent(slug);
+
+    const bookmarks = await VerifyToken(decodedSlug, searchParams?.category)
+    // console.log(bookmarks)
+
+    // console.log('slug ', decodedSlug, 'category ', searchParams?.category)
+    return (
+        <div className=" border-black stroke-border flex flex-row ">
+            {/* <BookmarkSidebar /> */}
+            <main className="border-white border-0 px-2 w-full h-full my-14 mx-4 sm:mx-0 sm:my-16">
+                {/* <Navbar /> */}
+                <div className="flex flex-row place-items-center p-4 gap-4 mb-2">
+                    <Folder className="lg:h-12 lg:w-12 h-10 w-10" />
+                    <h1 className="sm:text-3xl  font-semibold text-2xl truncate">
+                        Category : {searchParams?.category}
+                    </h1>
+                </div>
+                {
+                    bookmarks.length > 0 ?
+                        bookmarks.map((val: fetchBookmark, key: number) => {
+                            const temp = val.link.substr(0, 100) + "...";
+                            val.link = val.link.length > 100 ? temp : val.link;
+                            return (
+                                <div key={val.id} className="bg-background p-4 border-primary border rounded-md flex flex-col gap-4 mx-4 mb-4 md:mb-8 ">
+                                    <div className="flex justify-between gap-4 sm:gap-8">
+                                        {/* metadata  */}
+                                        <a href={val.link} target="_blank" className="scroll-m-20 text-xl font-semibold tracking-tight break-all">
+                                            {val.metadata}
+                                        </a>
+
+
+                                    </div>
+                                    {/* link  */}
+                                    <a href={val.link} target="_blank" className="text-sm font-medium leading-none break-all">{val.link}</a>
+                                    {/* category  */}
+                                    <Link href={`/category/${val.categories}`}>
+                                        <Button key={val.id} size="sm" className="max-w-max border-lg border-primary text-primary-foreground text-xs sm:text-base">{val.categories}</Button>
+                                    </Link>
+                                    <div className="flex justify-between items-center">
+
+                                        <div className="flex flex-wrap gap-4">
+                                            {val.labels ? val.labels.map((value, key) => {
+                                                const temp = value.text.substr(0, 10) + "...";
+                                                value.text = value.text.length > 8 ? temp : value.text;
+
+                                                return (<div key={key} className="bg-secondary text-secondary-foreground border px-2 py-1 max-w-max rounded-lg text-xs sm:text-base">
+                                                    {value.text}
+                                                </div>)
+                                            }
+                                            ) : ""}
+                                        </div>
+                                    </div>
+                                </div>
+                            )
+                        })
+                        :
+                        <div className="bg-background p-4 border-primary border rounded-md flex flex-col gap-4 mx-4 mb-4 md:mb-8 ">
+                            <h5 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
+                                Looks like nothing here ...
+                            </h5>
+                        </div>
+                }
+            </main>
+        </div>
+    );
+}
